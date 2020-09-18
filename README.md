08f44e222e9a2a3c21c3c76ebc90666520c666ec
Michael Joseph <michaeljoseph195@gmail.com>
2020-09-18 14:53:33 +0200

    create repo

---
+++ README.md
@@ -0,0 +1,3 @@
+### Tetris Game
+
+To practice C++ I'm working on this little tetris console game.
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <ncurses.h>
+
+
+void setup();
+void draw();
+void input();
+void logic();
+
+
+int main()
+{
+    
+
+    return 0;
+}
